a1abc7aabdf34212601dfb9ffbda07fc490b5a58
Justin <jurbin6090@gmail.com>
2018-02-12 19:59:33 -0800

    Reorganized components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import {NgModule} from '@angular/core';
 
 
 import {AppComponent} from './app.component';
-import {CharacterListComponent} from './character-list/character-list.component';
-import {CharacterCreateComponent} from './character-create/character-create.component';
-import {PlayerCreateComponent} from './player-create/player-create.component';
-import {PlayerListComponent} from './player-list/player-list.component';
+import {CharacterListComponent} from './character/character-list/character-list.component';
+import {CharacterCreateComponent} from './character/character-create/character-create.component';
+import {PlayerCreateComponent} from './player/player-create/player-create.component';
+import {PlayerListComponent} from './player/player-list/player-list.component';
 import {GameListComponent} from './game/game-list/game-list.component';
 import {GameCreateComponent} from './game/game-create/game-create.component';
 import {GameService} from "./game/game.service";
